9eb291166db138549652080f8b4a1624ba9845bd
Sam <skk1@cmu.edu>
2014-11-13 15:40:53 -0500

    got wizard to appear on menu call

--- src/privacypolicystatementgenerator/actions/OpenPrivacyPolicyStatementGeneratorViewActionDelegate.java
+++ src/privacypolicystatementgenerator/actions/OpenPrivacyPolicyStatementGeneratorViewActionDelegate.java
@@ -2,10 +2,13 @@ package privacypolicystatementgenerator.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PartInitException;
+
+import wizards.CaptureInfoWizard;
 
 /** 
  * This class is the action that is triggered when clicking the menu to open
@@ -27,6 +30,11 @@ public class OpenPrivacyPolicyStatementGeneratorViewActionDelegate implements
     
 	@Override
 	public void run(IAction action) {
+        CaptureInfoWizard wizard = new CaptureInfoWizard();
+        WizardDialog dialog = new WizardDialog(window.getShell(), wizard);
+        dialog.create();
+        dialog.open();
+		/*
 		IWorkbenchPage page = window.getActivePage();
 
         try {
@@ -36,6 +44,7 @@ public class OpenPrivacyPolicyStatementGeneratorViewActionDelegate implements
         } catch (PartInitException e) {
 
         }
+        */
 	}
 
 	@Override
